a1e41e85fde6c64b192a49071c077f08c3b6b658
Jaison Peres <jaisoncperes@gmail.com>
2021-07-07 10:30:37 -0300

    Improve documentation and add more .env vars
    Signed-off-by: Jaison Peres <jaisoncperes@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,17 @@ import cors from 'cors'
 import axios from 'axios'
 
 const app = express()
+const port = process.env.PORT || 3000
 
+// Configure settings of final API with .env file
 const apiUrl = process.env.API_URL || 'http://api.example.com'
 const credentials = process.env.API_TOKEN || 'token_example'
-const apiName = process.env.API_NAME
+const apiName = process.env.API_NAME || 'API_EXAMPLE'
+const apiAuthPath = process.env.API_AUTH_PATH || '/path/to/auth'
+const proxyToken = process.env.API_AUTH_PATH || '123456'
 
+
+// Instance of axios with API_URL
 const apiCrm = axios.create({
   baseURL: apiUrl,
   headers: {
@@ -17,10 +23,11 @@ const apiCrm = axios.create({
   }
 })
 
-async function getCrmToken () {
+// Authenticate proxy service on final api
+async function authProxyOnApi () {
   try {
     const { data: { nmToken } } = await apiCrm.post(
-      '/kernel/security/Token',
+      apiAuthPath,
       {
         dsCredentials: credentials
       }
@@ -32,12 +39,10 @@ async function getCrmToken () {
   }
 }
 
-const port = 3000
-
-const token = 'hk290120'
-
+// Set CORS options
 const corsOptions = {
   exposedHeaders: ['Content-Type', 'Cache-Control'],
+  // Inclute more methods to allow
   methods: ['GET', 'POST']
 }
 
@@ -51,20 +56,22 @@ app.use('*', async (req, res) => {
     return
   }
   
-  // Validate proxy authorization token
-  if(!req.headers.authorization || req.headers.authorization !== token) {
+  // Validate proxy service token
+  if(!req.headers.authorization || req.headers.authorization !== proxyToken) {
     res.sendStatus(403)
     return
   }
 
-  // Get CRM api token
-  const crmToken = await getCrmToken()
-  console.log(crmToken)
+  // Get token from final api authentication
+  const finalToken = await authProxyOnApi()
+  console.log(finalToken)
 
   // Parse proxy parameters to request CRM Api
   const { headers, method, body, originalUrl } = req
   console.log('request params: ', { headers, originalUrl, method, body })
 
+  // Execute request on final api
+  // TODO: improve authentication header options
   try {
     const response = await apiCrm[method.toLowerCase()](
       originalUrl,
@@ -72,7 +79,7 @@ app.use('*', async (req, res) => {
       {
         withCredentials: true,
         headers: {
-          'Cookie': `crmAuthToken=${crmToken}`
+          'Cookie': `crmAuthToken=${finalToken}`
         }
       }
     )
--- src/user.ts
+++
@@ -1,5 +0,0 @@
-export class User {
-    public id: string
-    public name: string
-    public email: string
-}
